074591497f20e3a810b21af905285d80c9366ac8
Takeda <ydetakeda27@gmail.com>
2023-04-06 05:45:57 +0300

    Added the RandomCreator script, to randomly generate objects falling from sky.

--- Assets/RandomCreator.cs
+++ Assets/RandomCreator.cs
@@ -5,28 +5,56 @@ using UnityEngine;
 public class RandomCreator : MonoBehaviour
 {
     [SerializeField] private Transform creationOrigin;
+    [SerializeField] private List<GameObject> objectsToCreate = new List<GameObject>();
 
     private enum RandomState {oneSided, twoSided}
-    [SerializeField] RandomState randomizerMethod = RandomState.twoSided;
+    [SerializeField] private RandomState randomizerMethod = RandomState.twoSided;
     [SerializeField] private Vector3 randomizerOffset;
     [SerializeField] private Vector2 randomizedDelay;
     [SerializeField] private float destroyTime = 10f;
-    [SerializeField] private List<GameObject> objectsToCreate = new List<GameObject>();
+
+    private IEnumerator coroutine;
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        coroutine = CreateRandomized(objectsToCreate, creationOrigin, randomizerMethod, randomizerOffset, randomizedDelay, destroyTime);
+        StartCoroutine(coroutine);
     }
 
-    private IEnumerator CreateRandomized(List<GameObject> objectsToCreate, Transform creationOrigin, RandomState randomizerMethod, Vector2 randomizedDelay)
+    private void Update()
+    {
+    }
+
+    private IEnumerator CreateRandomized(List<GameObject> objectsToCreate, Transform creationOrigin, RandomState randomizerMethod, Vector3 randomizerOffset, Vector2 randomizedDelay, float destroyTime = 0f)
     { 
         GameObject instantiated;
+        Vector3 randomizedPosition = Vector3.zero;
 
-        foreach (GameObject gameobject in objectsToCreate)
+        while (true)
         {
-            instantiated = Instantiate(gameobject);
-            yield return new WaitForSecondsRealtime(Random.Range(randomizedDelay.x + 0.1f, randomizedDelay.y));
-        }
+            yield return new WaitUntil(() => !GameState.IsPaused());
+
+            foreach (GameObject gameobject in objectsToCreate)
+            {
+                if (randomizerMethod == RandomState.oneSided)
+                {
+                    randomizedPosition.x = Random.Range(0f, randomizerOffset.x);
+                    randomizedPosition.y = Random.Range(0f, randomizerOffset.y);
+                    randomizedPosition.z = Random.Range(0f, randomizerOffset.z);
+                }
+                else
+                {
+                    randomizedPosition.x = Random.Range(-randomizerOffset.x, randomizerOffset.x);
+                    randomizedPosition.y = Random.Range(-randomizerOffset.y, randomizerOffset.y);
+                    randomizedPosition.z = Random.Range(-randomizerOffset.z, randomizerOffset.z);
+                }
+
+                instantiated = Instantiate(gameobject, creationOrigin.position + randomizedPosition, Quaternion.identity, creationOrigin);
+                if (destroyTime > 0f) { Destroy(instantiated, destroyTime); }                
+            }
+
+            yield return new WaitForSecondsRealtime(Random.Range(randomizedDelay.x, randomizedDelay.y));
+        }    
     }
 }
